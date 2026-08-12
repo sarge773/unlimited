955ab3f7563fd2e60545fbb4bec5b4bff5f9a996
mohammadsaleh40 <mohammadsalehmohammadsaleh@gmail.com>
2026-08-12 09:18:10 +0330

    fix(client): accept setState dispatcher in SegmentedControl onValueChange

--- client/src/components/ui/segmented-control.tsx
+++ client/src/components/ui/segmented-control.tsx
@@ -16,7 +16,7 @@ export function SegmentedControl<T extends string>({
   className,
 }: {
   value: T
-  onValueChange: (value: T) => void
+  onValueChange: React.Dispatch<React.SetStateAction<T>>
   options: SegmentOption<T>[]
   ariaLabel?: string
   className?: string
